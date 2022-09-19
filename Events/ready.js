d89b1b0de85763590368031088628559b73968e1
Witrold <69168507+Witrold@users.noreply.github.com>
2022-09-19 23:16:24 +0200

    Acctivity
    misse en place de acctivity

--- Events/ready.js
+++ Events/ready.js
@@ -1,9 +1,11 @@
 const Discord = require('discord.js')
 const loadSlashCommands = require('../Loaders/loadSlashCommands')
 
-module.exports = async bot => {
+module.exports = async (bot, client) => {
 
     await loadSlashCommands(bot)
 
     console.log(`${bot.user.tag} est bien en ligne`)
+
 }
+
--- Loaders/loadSlashCommands.js
+++ Loaders/loadSlashCommands.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js')
 const { REST } = require('@discordjs/rest')
-const { Routes} = require('discord.js');
+const { Routes} = require('discord.js')
+const{ ActivityType } = require('discord.js');
 
 module.exports = async bot => {
     
@@ -25,6 +26,15 @@ module.exports = async bot => {
 
     const rest = new REST({version: "10"}).setToken(bot.token)
 
+    //////////////////////////////////////////////////////REGARDE///////////////////////////////////////////////////
+    
     await rest.put(Routes.applicationCommands(bot.user.id), {body: commands})
-    console.log("les slash command on été créées")
+    setInterval(async () => {
+        const Statut_Random_AutoChange = [`${bot.users.cache.size} membres`,`${bot.guilds.cache.size} serveurs`][Math.floor(Math.random() * [`${bot.guilds.cache.size} serveurs`,`${bot.users.cache.size} membres`].length)];
+        bot.user.setPresence({activities: [{name: `${Statut_Random_AutoChange}`, type: ActivityType.Watching}]})
+    }, 5000);
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    console.log("Les SlashCommands sont chargé avec succès.")
 }
